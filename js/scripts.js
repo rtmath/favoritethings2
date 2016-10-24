20aefe380a0a8b9e4978f17619e21b58cda4cd24
Eric Raetz and Ryan Mathisen <eric@>
2016-10-24 09:38:37 -0700

    add final list display

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $("document").ready(function() {
   var favoriteHobby = [];
   var favoriteOS = [];
   var favoriteThings = [];
+  var partialArray = [];
   $("#myForm").submit( function(event) {
     event.preventDefault();
     favoriteFood.push($("#food").val());
@@ -17,5 +18,13 @@ $("document").ready(function() {
   });
   $("#test").click( function() {
     console.log(favoriteThings);
+    partialArray.push(favoriteThings[1]);
+    partialArray.push(favoriteThings[0]);
+    partialArray.push(favoriteThings[2]);
+    console.log(partialArray);
+    $("#fav1").text(partialArray[0]);
+    $("#fav2").text(partialArray[1]);
+    $("#fav3").text(partialArray[2]);
+    $("#display").show();
   });
 });
